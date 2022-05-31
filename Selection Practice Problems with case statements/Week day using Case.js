756bfee721a20952b1ba044c8844cabcf3e65738
Yogesh More <iyogesh.more1998@gmail.com>
2022-05-31 15:10:11 +0530

    Added : Read a Number and Display the week day using case.

---
+++ Selection Practice Problems with case statements/Week day using Case.js
@@ -0,0 +1,38 @@
+// 2. Read a Number and Display the week day (Sunday, Monday,...)
+
+const prompt = require("prompt-sync")();
+let days = parseInt(prompt("Enter number between 0 to 6: "));
+
+switch (days) {
+    case 0:
+        console.log("Sunday");
+    break;
+    
+    case 1:
+        console.log("Monday");
+    break;
+
+    case 2:
+        console.log("Tuesday");
+    break;
+
+    case 3:
+        console.log("Wednesday");
+    break;
+
+    case 4:
+        console.log("Thursday");
+    break;
+
+    case 5:
+        console.log("Friday");
+    break;
+
+    case 6:
+        console.log("Saturday");
+    break;
+
+    default:
+        console.log("Invalid input.");
+    break;
+}
