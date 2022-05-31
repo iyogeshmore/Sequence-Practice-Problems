78cd0667c237c0d0c136ade591d01256fb92769c
Yogesh More <iyogesh.more1998@gmail.com>
2022-05-31 10:39:39 +0530

    Added : coin flip and print out Heads or Tails accordingly.

---
+++ Selection Practice Problems with if & else/flipCoin.js
@@ -0,0 +1,12 @@
+// 4. Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly.
+
+let num = Math.random();
+
+if (num < 0.5) 
+{
+  console.log("Heads");
+}
+else 
+{
+  console.log("Tails");
+}
